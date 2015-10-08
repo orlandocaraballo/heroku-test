01996923283812b63333be1d9a8116ea9cd26630
Orlando Caraballo <orlando@nycda.com>
2015-10-08 08:50:40 -0400

    new files

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 require "sinatra"
 require "sinatra/activerecord"
+require "./models"
 
 configure :development do
   set :database, "sqlite3:heroku-test.sqlite3"
 end
 
 get "/" do
-  "display some stuff"
+  @users = User.all
+  erb :index
+end
+
+get "/users/create" do
+  User.create(
+    first_name: "Orlando",
+    last_name: "Caraballo",
+    birthday: "12/1/1983"
+  )
 end
